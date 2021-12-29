e93a476aeeafaa5dfec032a1355978d3adb2f917
IsolatedLun <herojoe210@gmail.com>
2021-12-29 18:44:02 +0200

    Made Navbar + responsive

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='primary-nav'>
-            <div className="nav__head">
-                <img src="./src/images/logo_small.png" className='nav__icon' />
+        <nav className='primary-nav flex--align--between'>
+            <div className="nav__head flex--align gap--1">
+                <div className="nav__icon"><img src="./src/images/logo_small_v2.png" /></div>
                 <h1 className="nav__title">Musicify</h1>
             </div>
+
+            <ul className="nav__links flex--align gap--1">
+                <li className="nav__link"><a href="#">Home</a></li>
+                <li className="nav__link"><a href="#">Browse</a></li>
+                <li className="link--split"></li>
+                <li className='nav__link'><a href="#">Log in</a></li>
+                <li className="nav__link"><a href="#">Sign up</a></li>
+            </ul>
+
+            <button className='fa nav__dropdown btn--def'>&#xf0c9;</button>
         </nav>
     )
 }
