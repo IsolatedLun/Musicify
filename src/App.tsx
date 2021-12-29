1d858936b431e3955f717d487f7c86aa99afe049
IsolatedLun <herojoe210@gmail.com>
2021-12-29 17:09:21 +0200

    Added postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import Navbar from "./components/layout/Navbar"
 
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+    <Navbar />
   )
 }
 
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className='primary-nav'>
+            <div className="nav__head">
+                <img src="./src/images/logo_small.png" className='nav__icon' />
+                <h1 className="nav__title">Musicify</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
